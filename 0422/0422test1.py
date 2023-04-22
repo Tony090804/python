ffa17f4c64e1fd1b00203b5007d555084ad50c75
Tony090804 <130810983+Tony090804@users.noreply.github.com>
2023-04-22 12:39:21 +0900

    Add files via upload

---
+++ 0422/0422test1.py
@@ -0,0 +1,17 @@
+'''
+2023.04.22
+장우혁
+변수, 연산자 복습하기
+'''
+num1=12 #num1에 숫자 저장하기
+num2=num1+10 #num2에 num1에 10 더한 값 저장하기
+ans1=num2%2 #ans1에 num2에 2를 나눈 뒤 나운 나머지 값 저장하기
+
+print ('"I like python"') # I like python 출력하기
+print('num2를 2로 나눈 나머지는 ',ans1)
+print('num2를 2로 나눈 몫은 ',num1//2)
+
+str1='덩덕'+'쿵덕' #str1에 덩덕쿵덕 저장하기
+str2=('쿵덕')*2 #str2에 쿵덕쿵덕 저장하기
+jajinmori=(str1+str2)*2 #jajinmori에 str1+str2를 2번 반복한 값 저장하기
+print(jajinmori)
---
+++ 0422/0422test3.py
@@ -0,0 +1,27 @@
+'''
+2023.04.22
+장우혁
+#문제
+    num1, num2를 이용한 사칙연산 프로그램
+#문제분석
+    변수
+    수식
+#알고리즘
+    1.변수선언
+        num1=10
+        num2=20  
+    2.사칙연산 계산
+        print('{}+{}={}'.format(num2,num1,num2+num1))
+        print('{}-{}={}'.format(num2,num1,num2-num1))
+        print('{}*{}={}'.format(num2,num1,num2*num1))
+        print('{}/{}={}'.format(num2,num1,num2/num1))
+'''
+
+num1=10 #정수10
+num2=20 #정수20
+
+print('{}+{}={}'.format(num2,num1,num2+num1))
+print('{}-{}={}'.format(num2,num1,num2-num1))
+print('{}*{}={}'.format(num2,num1,num2*num1))
+print('{}/{}={}'.format(num2,num1,num2/num1))
+
